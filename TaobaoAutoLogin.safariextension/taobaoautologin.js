db218cddb0ffa80f0d852c34d189bdda59078adb
Alex Rezit <AlexRezit@gmail.com>
2013-06-29 22:14:13 +0800

    Add Alipay login support.

--- TaobaoAutoLogin.safariextension/taobaoautologin.js
+++ TaobaoAutoLogin.safariextension/taobaoautologin.js
@@ -14,10 +14,15 @@ var checkSafeLoginStatusInt = window.setInterval('checkSafeLoginStatus()', 10);
 function checkSafeLoginStatus() {
     var safeLoginCheckbox = document.getElementById('J_SafeLoginCheck');
     var isSafeLogin = safeLoginCheckbox.checked;
+
+    // If login box is loaded.
     if (document.getElementById('J_LoginBox').className.indexOf('loading') < 0) {
+
         if (isSafeLogin) {
+            // Disable safe login option.
             safeLoginCheckbox.click();
         } else {
+            // Input info and login.
             checkSafeLoginStatusInt = window.clearInterval(checkSafeLoginStatusInt);
             document.getElementById('TPL_username_1').value = settings.username;
             document.getElementById('TPL_password_1').value = settings.password;
@@ -25,4 +30,8 @@ function checkSafeLoginStatus() {
         }
     }
 }
+
+// Alipay login via Taobao.
+document.getElementsByClassName('aliLogin-icon-taobao')[0].click();
+document.getElementsByClassName('auth-taobaoMember')[0].click();
     
